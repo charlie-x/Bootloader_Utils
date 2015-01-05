0912c92b87a39db09f9edf36594fea94461a18f7
charliex <charlie@finitemonkeys.com>
2015-01-04 16:14:51 -0800

    tidy.

--- Bootloader_Utils/main.cpp
+++ Bootloader_Utils/main.cpp
@@ -5,15 +5,91 @@
 #include "cybtldr_api.h"
 #include "cybtldr_api2.h"
 
-HANDLE hComm;
+static HANDLE hComm;
+static TCHAR gszPort[26] = L"COM2";
 
+
+VOID CommSetBaud(VOID)
+{
+	if (hComm != NULL) {
+
+
+		DCB dcb;
+
+		ZeroMemory(&dcb, sizeof(dcb));
+		dcb.DCBlength = sizeof(dcb);
+
+		dcb.BaudRate = CBR_115200;
+		dcb.fBinary = TRUE;
+		dcb.fParity = TRUE;
+		dcb.fOutxCtsFlow = FALSE;
+		dcb.fOutxDsrFlow = FALSE;
+		dcb.fDtrControl = DTR_CONTROL_DISABLE;
+		dcb.fDsrSensitivity = FALSE;
+		dcb.fOutX = FALSE;
+		dcb.fErrorChar = FALSE;
+		dcb.fNull = FALSE;
+		dcb.fRtsControl = RTS_CONTROL_DISABLE;
+		dcb.fAbortOnError = FALSE;
+		dcb.ByteSize = 8;
+		dcb.Parity = NOPARITY;
+		dcb.StopBits = ONESTOPBIT;
+
+		SetCommState(hComm, &dcb);
+	}
+	return;
+}
+
+int SetTimeouts(void)
+{
+	COMMTIMEOUTS timeouts;
+
+	if (hComm == NULL) return 1;
+
+	timeouts.ReadIntervalTimeout = 1;
+	timeouts.ReadTotalTimeoutMultiplier = 1;
+	timeouts.ReadTotalTimeoutConstant = 1;
+	timeouts.WriteTotalTimeoutMultiplier = 1;
+	timeouts.WriteTotalTimeoutConstant = 1;
+
+	if (!SetCommTimeouts(hComm, &timeouts)) {
+		return 1;
+	}
+	return 0;
+
+}
 static int OpenConnection(void)
 {
+	hComm = CreateFile(gszPort,
+	                   GENERIC_READ | GENERIC_WRITE,
+	                   0,
+	                   NULL,
+	                   OPEN_EXISTING,
+	                   FILE_ATTRIBUTE_NORMAL | FILE_FLAG_OVERLAPPED,
+	                   NULL
+	                  );
+
+	if (hComm == INVALID_HANDLE_VALUE) {
+		// error opening port; abort
+
+		CloseHandle(hComm);
+
+		return 1;
+	}
+
+	CommSetBaud();
+
+	SetTimeouts();
+
 	return 0;
 }
 
 static int CloseConnection(void)
 {
+
+	CloseHandle(hComm);
+	hComm = 0;
+
 	return 0;
 }
 
@@ -76,48 +152,6 @@ static int ReadData(unsigned char *data, int count)
 	return 1;
 }
 
-static int ReadDataX(unsigned char *data, int count)
-{
-	DWORD dwCommEvent;
-	DWORD dwRead;
-	char  chRead;
-	unsigned int index = 0;
-
-	if (!SetCommMask(hComm, EV_RXCHAR)) {
-		// Error setting communications event mask
-		return 1;
-	}
-
-	memset(data, 0, count);
-
-
-	for (;;) {
-		if (WaitCommEvent(hComm, &dwCommEvent, NULL)) {
-			do {
-				if (ReadFile(hComm, &chRead, 1, &dwRead, NULL)) {
-					// A byte has been read; process it.
-					data[index++] = chRead;
-
-					if (index == count)
-						return 0;
-				} else {
-					error_display();
-					printf("GetLastError %d\n", GetLastError());
-					return 1;
-					// An error occurred in the ReadFile call.
-				}
-				break;
-			} while (dwRead);
-		} else
-			// Error in WaitCommEvent
-			return 1;
-		break;
-
-	}
-
-	return 0;
-
-}
 
 BOOL WriteABuffer(unsigned char * lpBuf, DWORD dwToWrite)
 {
@@ -193,80 +227,12 @@ static void ProgressUpdate(unsigned char arrayId, unsigned short rowNum)
 	printf("\t\tCompleted array %d, row %d    \r", arrayId, rowNum);
 }
 
-TCHAR gszPort[] = L"COM2";
 
-VOID CommSetBaud(VOID)
-{
-	if (hComm != NULL) {
-
-
-		DCB dcb;
-
-		ZeroMemory(&dcb, sizeof(dcb));
-		dcb.DCBlength = sizeof(dcb);
-
-		dcb.BaudRate = CBR_115200;
-		dcb.fBinary = TRUE;
-		dcb.fParity = TRUE;
-		dcb.fOutxCtsFlow = FALSE;
-		dcb.fOutxDsrFlow = FALSE;
-		dcb.fDtrControl = DTR_CONTROL_DISABLE;
-		dcb.fDsrSensitivity = FALSE;
-		dcb.fOutX = FALSE;
-		dcb.fErrorChar = FALSE;
-		dcb.fNull = FALSE;
-		dcb.fRtsControl = RTS_CONTROL_DISABLE;
-		dcb.fAbortOnError = FALSE;
-		dcb.ByteSize = 8;
-		dcb.Parity = NOPARITY;
-		dcb.StopBits = ONESTOPBIT;
-
-		SetCommState(hComm, &dcb);
-	}
-	return;
-}
-
-int set_timeouts(void)
-{
-	COMMTIMEOUTS timeouts;
-
-	if (hComm == NULL) return 1;
-
-	timeouts.ReadIntervalTimeout = 1;
-	timeouts.ReadTotalTimeoutMultiplier = 1;
-	timeouts.ReadTotalTimeoutConstant = 1;
-	timeouts.WriteTotalTimeoutMultiplier = 1;
-	timeouts.WriteTotalTimeoutConstant = 1;
-	if (!SetCommTimeouts(hComm, &timeouts)) {
-		return 1;
-	}
-	return 0;
-
-}
 
 int main(int argc, char* argv[])
 {
 
-	hComm = CreateFile(gszPort,
-	                   GENERIC_READ | GENERIC_WRITE,
-	                   0,
-	                   NULL,
-	                   OPEN_EXISTING,
-	                   FILE_ATTRIBUTE_NORMAL | FILE_FLAG_OVERLAPPED,
-	                   NULL
-	                  );
 
-	if (hComm == INVALID_HANDLE_VALUE) {
-		// error opening port; abort
-
-		CloseHandle(hComm);
-
-		exit(1);
-	}
-
-	CommSetBaud();
-
-	set_timeouts();
 
 	CyBtldr_CommunicationsData cyComms = {
 		&OpenConnection,
@@ -285,7 +251,7 @@ int main(int argc, char* argv[])
 
 	printf("\nresult = %d\n", result);
 
-	CloseHandle(hComm);
+
 
 	return result;
 }
